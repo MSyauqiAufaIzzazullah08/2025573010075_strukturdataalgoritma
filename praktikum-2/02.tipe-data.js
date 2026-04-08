d8489af4e7f307a52174b1b7c991375401c05f35
MSyauqiAufaIzzazullah08 <kohanealter@gmail.com>
2026-04-08 15:37:04 +0700

    Praktikum 2: Dasar JavaScript - variabel, tipe data, operator, kondisional, perulangan

---
+++ praktikum-2/02.tipe-data.js
@@ -0,0 +1,33 @@
+let namaMahasiswa = 'Ahmad Fauzi';
+let programStudi = "Teknik Informatika";
+
+let perkenalan = 'Halo! Nama saya ${namaMahasiswa} dari ${programStudi}.';
+console.log(perkenalan);
+console.log('Panjang nama:', namaMahasiswa.lenght);
+
+let nilaiUjian = 87;
+let ipk        = 3.75;
+let suhuKulkas = -4;
+
+console.log('Nilai Ujian :', nilaiUjian);
+console.log('IPK         :', ipk);
+console.log('Suhu Kulkas :', suhuKulkas);
+
+let sudahlogin   = true;
+let sudahlulus   = false;
+
+console.log('Sudah login :', sudahlogin);
+console.log('Sudah lulus :', sudahlulus);
+
+let fotoProfil = null; 
+console.log('Foto profil:', fotoProfil);
+
+let nomorTelepon;
+console.log('No. Telepon:', nomorTelepon);
+
+console.log('--- Tipe Data ---');
+console.log('namaMahasiswa :', typeof namaMahasiswa);
+console.log('nilaiujian    :', typeof nilaiUjian);
+console.log('sudahlogin    :', typeof sudahlogin);
+console.log('fotoProfil    :', typeof fotoProfil);
+console.log('nomorTelepon  :', typeof nomorTelepon);
